1692c4fe8653ec8ccbb563fcf1cf201e6f76a936
Vishal-pandey17 <vishalpandey86681@gmail.com>
2023-02-06 22:28:52 +0530

    Create first cart succesfully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import React  from 'react';
+import CartItem from './CartItem';
 
 function App() {
   return (
     <div className="App">
       <h1>Cart</h1>
+        <CartItem />
     </div>
   );
 }
