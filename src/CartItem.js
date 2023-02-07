81218a6970e92985021a6c3f969ffe512ffc04dc
Vishal-pandey17 <vishalpandey86681@gmail.com>
2023-02-07 17:20:26 +0530

    Add increase quantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor(){
+        super();// this will call the constructor of component class 
+        this.state = {
+            price: 999,
+            title: "PHONE",
+            qty: 1,
+            img: ' '
+        }
+    } 
+    increaseQuantity(){
+        console.log(this.state);
+    }
     render () {
+        const {price, title, qty} = this.state // object destructuring
       return (
           <div className='cart-item'>
             <div className='left-block'>
@@ -9,16 +22,18 @@ class CartItem extends React.Component {
             </div>
             <div className='right-block'>
               <div style={{ fontSize: 25 }}>
-                 Phone
+                 {this.state.title}
               </div>
               <div style={{color: '#777'}}>
-                 RS 999
+                 Rs {price}
               </div>
               <div style={{color: '#777'}}>
-                 Qty: 1
+                 Qty: {qty}
               </div> 
               <div className='cart-item-actions'>
-
+               <img alt='Increase' className='action-icons' src='https://t3.ftcdn.net/jpg/03/30/25/74/240_F_330257485_WEd8LDmLkdp4vbJjhbLY263gV0OvsiIF.jpg' onClick={this.increaseQuantity.bind(this)}/>
+               <img alt='Decrease' className='action-icons' src='https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg' />
+               <img alt='delete' className='action-icons' src='https://t4.ftcdn.net/jpg/01/90/89/15/240_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg' />
               </div>
             </div>
           </div>
