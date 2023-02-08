db2c2b4ecd709a75bac8769f518d4dfe0fba594f
Vishal-pandey17 <vishalpandey86681@gmail.com>
2023-02-08 23:30:38 +0530

    Adding more cart successfully

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React  from 'react';
 import CartItem from './CartItem';
+import Cart from './Cart';
 
 function App() {
   return (
     <div className="App">
       <h1>Cart</h1>
         <CartItem />
+        <Cart/>
     </div>
   );
 }
--- src/CartItem.js
+++ src/CartItem.js
@@ -7,19 +7,33 @@ class CartItem extends React.Component {
             price: 999,
             title: "PHONE",
             qty: 1,
-            img: ' '
+            img: ''
         }
     } 
     increaseQuantity = () =>{
         //this.state.qty += 1;
         console.log(this.state);
-        /*this.setState({
+        /*this.setState({                         // This will bundle all the state as one using batching and render only once.
             qty: this.state.qty+1
         })*/
+        
         // Another Method
-        this.setState((prevState)=>{
+        this.setState((prevState)=>{              // this wil store in queue format and also using batching and render only once.
             return {
                qty: prevState.qty+1
+            }
+        });
+      
+    }
+    decreaseQuantity = () =>{
+        const {qty} = this.state;  // object destructuring
+        if(qty == 0){
+            return;
+        }
+
+        this.setState((prevState)=>{
+            return {
+               qty: prevState.qty-1
             }
         });
     }
@@ -42,7 +56,7 @@ class CartItem extends React.Component {
               </div> 
               <div className='cart-item-actions'>
                <img alt='Increase' className='action-icons' src='https://t3.ftcdn.net/jpg/03/30/25/74/240_F_330257485_WEd8LDmLkdp4vbJjhbLY263gV0OvsiIF.jpg' onClick={this.increaseQuantity.bind(this)}/>
-               <img alt='Decrease' className='action-icons' src='https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg' />
+               <img alt='Decrease' className='action-icons' src='https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg'  onClick={this.decreaseQuantity.bind(this)}/>
                <img alt='delete' className='action-icons' src='https://t4.ftcdn.net/jpg/01/90/89/15/240_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg' />
               </div>
             </div>
